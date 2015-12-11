3f9d81c858bfd4745bb3a7b08c02897fd311f596
Andros <andros.tjandra@gdplabs.id>
2015-12-11 13:38:46 +0700

    add grouping example to execute certain rules only

--- easy-rules-example/src/main/java/org/easyrulestest/Main.java
+++ easy-rules-example/src/main/java/org/easyrulestest/Main.java
@@ -1,16 +1,20 @@
 package org.easyrulestest;
 
+import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.easyrules.api.RulesEngine;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void testLeaveEmployee() {
         // here I compare 2 different rule engines //
-        
+
         Employee arturia = new Employee("arturia", new Date(15, 10, 2015));
         Employee nero = new Employee("nero", new Date(16, 10, 2015));
 
@@ -27,8 +31,7 @@ public class Main {
         kSession.fireAllRules();
         System.out.println(leaveArturia.getEmployee() + " " + leaveArturia.getStatus());
         System.out.println(leaveNero.getEmployee() + " " + leaveNero.getStatus());
-        
-        
+
         // using easyrules //
         arturia = new Employee("arturia", new Date(15, 10, 2015));
         nero = new Employee("nero", new Date(16, 10, 2015));
@@ -40,7 +43,28 @@ public class Main {
         rulesEngine.fireRules();
         System.out.println(leaveArturia.getEmployee() + " " + leaveArturia.getStatus());
         System.out.println(leaveNero.getEmployee() + " " + leaveNero.getStatus());
-        
     }
+    public static void testGroup() {
+     // using drools //
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession("ksession-rules");
+        System.out.println("Run group string");
+        kSession.insert(new Integer(1));
+        kSession.insert(new Integer(2));
+        kSession.insert(new Integer(3));
+        kSession.getAgenda().getAgendaGroup("String").setFocus();
+        kSession.fireAllRules();
+        System.out.println("Run group math");
+        kSession.insert(new Integer(1));
+        kSession.insert(new Integer(2));
+        kSession.insert(new Integer(3));
+        kSession.getAgenda().getAgendaGroup("Math").setFocus();
+        kSession.fireAllRules();
+    }
+    public static void main(String[] args) {
+        //testLeaveEmployee();
+        testGroup();
+    }   
 
 }
